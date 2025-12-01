3a7d0175146ed51a4b346d960252120ea9862eac
kimdohwan7220 <dohwan8220@naver.com>
2025-12-01 15:49:27 +0900

    refactor: 24. th:with 적용하여 한번 split 한 결과를 변수화 하여 사용, 소스코드 가독성이 좋아짐

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -29,11 +29,11 @@ public class PostController {
     @AllArgsConstructor
     @Getter
     public static class WriteForm {
-        @NotBlank(message = "1-제목을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "2-제목은 2자 이상, 20자 이하로 입력가능합니다.")
+        @NotBlank(message = "01-title-제목을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "02-title-제목은 2자 이상, 20자 이하로 입력가능합니다.")
         private String title;
-        @NotBlank(message = "3-내용을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "4-내용은 2자 이상, 20자 이하로 입력가능합니다.")
+        @NotBlank(message = "03-content-내용을 입력해주세요.")
+        @Size(min = 2, max = 20, message = "04-content-내용은 2자 이상, 20자 이하로 입력가능합니다.")
         private String content;
     }
 
